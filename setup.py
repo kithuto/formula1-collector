2d5b23abe371647bdb77efe99df09a1d597bf8f7
Ignasi Rovira <32537423+kithuto@users.noreply.github.com>
2024-06-04 20:55:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
    name='formula1_collector',
    version='0.4.3',
    author='Ignasi Rovira',
-   url='https://github.com/kithuto/formula-1-collector',
+   url='https://github.com/kithuto/formula1-collector',
    packages=['formula1_collector'],
    license='MIT',
    description='A library for realtime formula 1 data',
